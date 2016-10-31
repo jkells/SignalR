63b5fd167b76bcb122b5d36c937b363c0d02644a
Jared Kells <jared@criticalarc.com>
2016-10-31 15:40:59 +1100

    Set a custom version number

--- build-ca.cmd
+++ build-ca.cmd
@@ -1,4 +1,10 @@
 @echo Off
+
+rem ****************************************************************************
+rem To update the version edit build\Microsoft.AspNet.SignalR.versions.targets
+rem ****************************************************************************
+
+
 rem Enforce package restore to avoid build issues. See http://go.microsoft.com/fwlink/?LinkID=317568 for more details
 msbuild .nuget\NuGet.targets /t:RestorePackages
 .nuget\nuget.exe restore %~dp0\src\Microsoft.AspNet.SignalR.Client.UWP\project.json
@@ -12,4 +18,4 @@ if "%config%" == "" (
    set config=Release
 )
 
-msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%";BUILD_NUMBER=5;PRERELEASE=false;Nightly=false /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
